6ae9f65acccf31131a6445ba72d91f982b19bac9
UniverseDevel <universedevel.help@outlook.com>
2021-09-19 12:23:53 +0200

    Changes

--- lights.php
+++ lights.php
@@ -102,6 +102,39 @@ if (@$_GET['c'] == 'l_g1_t') {
     } else {
         echo 'Error connecting to MQTT broker.<br>';
     }
+} else if (@$_GET['c'] == 'l_all_rtg') {
+    $message = '{
+    "mode": "white",
+    "red": 0,
+    "green": 0,
+    "blue": 0,
+    "gain": 100,
+    "brightness": 100,
+    "white": 0,
+    "temp": 3000,
+    "effect": 0,
+    "turn": "off"
+}';
+
+    $mqtt = new Bluerhinos\phpMQTT($MQTT_HOST, $MQTT_PORT, 'mqtt-forward-php_'.rand());
+    $mqtt->debug = false;
+    if ($mqtt->connect(true, null, $MQTT_USERNAME, $MQTT_PASSWORD)) {
+        $mqtt->publish('shellies/shellycolorbulb/living/1/01x01/color/0/set', $message, 0);
+        $mqtt->publish('shellies/shellycolorbulb/living/1/01x02/color/0/set', $message, 0);
+        $mqtt->publish('shellies/shellycolorbulb/living/2/01x03/color/0/set', $message, 0);
+        $mqtt->publish('shellies/shellycolorbulb/living/1/02x01/color/0/set', $message, 0);
+        $mqtt->publish('shellies/shellycolorbulb/living/1/02x02/color/0/set', $message, 0);
+        $mqtt->publish('shellies/shellycolorbulb/living/2/02x03/color/0/set', $message, 0);
+        $mqtt->publish('shellies/shellycolorbulb/living/1/03x01/color/0/set', $message, 0);
+        $mqtt->publish('shellies/shellycolorbulb/living/2/03x02/color/0/set', $message, 0);
+        $mqtt->publish('shellies/shellycolorbulb/living/2/03x03/color/0/set', $message, 0);
+        $mqtt->publish('shellies/shellycolorbulb/living/1/04x01/color/0/set', $message, 0);
+        $mqtt->publish('shellies/shellycolorbulb/living/2/04x02/color/0/set', $message, 0);
+        $mqtt->publish('shellies/shellycolorbulb/living/2/04x03/color/0/set', $message, 0);
+        $mqtt->close();
+    } else {
+        echo 'Error connecting to MQTT broker.<br>';
+    }
 } else {
 
 }
